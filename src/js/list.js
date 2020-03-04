a218adff7e21a94f48bf51896eded5528c049a3c
tang_jie <583359512@qq.com>
2020-03-04 18:19:41 +0800

    商品详情

---
+++ src/js/list.js
@@ -0,0 +1,104 @@
+$(function() {
+    $('.show').mouseover(function() {
+        $(this).children('div').stop().slideDown(300)
+    })
+    $('.show').mouseout(function() {
+        $(this).children('div').stop().slideUp(300)
+    })
+})
+getList()
+
+function getList() {
+    $.ajax({
+        url: '../lib/nav_top.json',
+        dataType: 'json',
+        success: function(res) {
+            console.log(res)
+            let str = ''
+            res.forEach(item => {
+                str += `<li><a href="#">${ item.name }</a></li>`
+            })
+            $('.nav-top > ul')
+                .html(str)
+                .on({
+                    mouseenter: () => $('.nav-box').stop().slideDown(),
+                    mouseleave: () => $('.nav-box').stop().slideUp()
+                })
+                .children('li')
+                .on('mouseover', function() {
+                    const index = $(this).index()
+                    const list = res[index].list
+                    let str = ''
+                    list.forEach(item => {
+                        str += `
+                            <li>
+                                <div>
+                                    <img src="${ item.list_url }" alt="">
+                                </div>
+                                <p class="title">${ item.list_name }</p>
+                                <span class="price">${ item.list_price }</span>
+                            </li>
+                                `
+                    })
+                    $('.nav-box > ul').html(str)
+                })
+            $('.nav-box')
+                .on({
+                    mouseover: function() {
+                        $(this).finish().show()
+                    },
+                    mouseout: function() {
+                        $(this).finish().slideUp()
+                    }
+                })
+        }
+    })
+}
+
+
+
+getList2()
+
+function getList2() {
+    $.ajax({
+        url: '../lib/list.json',
+        dataType: 'json',
+        success: function(res) {
+            $('.pagi').pagination({
+                pageCount: Math.ceil(res.length / 12),
+                current: 1,
+                jump: true,
+                coping: true,
+                homepage: '首页',
+                endPage: '末页',
+                prevContent: '<',
+                nextContent: '>',
+                callback: function(api) {
+                    let curr = api.getCurrent()
+                    var list = res.slice((curr - 1) * 12, curr * 12)
+                    bindHtml(list)
+                }
+            })
+            bindHtml(res.slice(0, 12))
+
+        }
+    })
+}
+
+function bindHtml(list) {
+    console.log(list)
+    let str = ''
+
+    list.forEach(item => {
+        str += `
+            <li>
+            <a href="">
+                <img src="${ item.url}" alt="">
+                <h3>${item.name}</h3>
+                <p>${ item.price}</p>
+                </a>
+            </li>
+                `
+    })
+    $('.box > ul').html(str)
+}
